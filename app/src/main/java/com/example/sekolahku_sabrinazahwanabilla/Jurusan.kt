cae5db32d8fe636b415bca371f66917e5f99eac6
Sabrina <SZNabilla@gmail.com>
2021-05-05 11:25:01 +0700

    Tambahan Dashboard Sekolahku 2

---
+++ app/src/main/java/com/example/sekolahku_sabrinazahwanabilla/Jurusan.kt
@@ -0,0 +1,12 @@
+package com.example.sekolahku_sabrinazahwanabilla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Jurusan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jurusan)
+    }
+}
---
+++ app/src/main/java/com/example/sekolahku_sabrinazahwanabilla/PPDB.kt
@@ -0,0 +1,12 @@
+package com.example.sekolahku_sabrinazahwanabilla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PPDB : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p_p_d_b)
+    }
+}
